47d693184d2d95d82f99dee8e6843aa55d075be8
Jeongmin Byun <jmbyun91@gmail.com>
2021-02-16 16:15:50 +0900

    finish skeleton for vue-based grouter compatible spa

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+        Hello world from Home view. ({{ text }})
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    props: {
+        text: String,
+    }
+}
+</script>
